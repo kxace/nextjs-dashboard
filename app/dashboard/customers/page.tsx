56bd094224072efce076d756f89d7fbdd06ccbc2
kxace <kuangxiao.ace@gmail.com>
2025-04-24 15:57:56 -0700

    add customer page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,8 +1,23 @@
-import { Metadata } from 'next';
+import CustomersTable from '@/app/ui/customers/table';
+import {Metadata} from 'next';
+import {Suspense} from 'react';
 
 export const metadata: Metadata = {
     title: 'Customers',
 };
-export default function Page() {
-    return <p>Customers Page</p>;
+
+export default async function CustomersPage(props: {
+        searchParams?: Promise<{
+            query?: string;
+        }>;
+    }) {
+    const searchParams = await props.searchParams;
+
+    const query = searchParams?.query || '';
+
+    return (
+        <Suspense fallback={<p>Loading...</p>}>
+            <CustomersTable query={query}/>
+        </Suspense>
+    )
 }
